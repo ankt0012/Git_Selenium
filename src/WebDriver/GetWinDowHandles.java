8dd08c8049c5c8704c324e033509db54c8bfe95e
Ankit Kumar <akrjee15@gmail.com>
2022-10-13 04:10:03 +0530

    Push getWindowHandles in WebDriver

---
+++ src/WebDriver/GetWinDowHandles.java
@@ -0,0 +1,30 @@
+package WebDriver;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetWinDowHandles {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.actitime.com/login.do");
+
+		driver.findElement(By.linkText("actiTIME Inc.")).click();
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		for (String id : allWindowIDs) {
+			driver.switchTo().window(id);
+			if (id.equals(allWindowIDs)) {
+				driver.findElement(By.linkText("Try Free")).click();
+				break;
+
+			}
+
+		}
+
+	}
+
+}
