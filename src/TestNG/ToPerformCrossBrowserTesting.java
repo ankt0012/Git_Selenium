d566a2d422cc40251a0f5829cf2524c602aa559b
Ankit Kumar <akrjee15@gmail.com>
2022-10-17 14:50:47 +0530

    ..

--- src/TestNG/ToPerformCrossBrowserTesting.java
+++ src/TestNG/ToPerformCrossBrowserTesting.java
@@ -2,6 +2,7 @@ package TestNG;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,7 +10,7 @@ import org.testng.annotations.Test;
 
 public class ToPerformCrossBrowserTesting {
 
-    WebElement driver;
+    WebDriver driver;
 
     @Test
     public void demo(String browser) {
